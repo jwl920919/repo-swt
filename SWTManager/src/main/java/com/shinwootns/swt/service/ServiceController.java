aa1b5e23c8b6b169bf61185866b73ee148d8ea0b
songagi <songagi@gmail.com>
2016-06-21 14:32:17 +0900

    Modified

--- SWTManager/src/main/java/com/shinwootns/swt/service/ServiceController.java
+++ SWTManager/src/main/java/com/shinwootns/swt/service/ServiceController.java
@@ -1,18 +1,26 @@
 package com.shinwootns.swt.service;
 
-/*
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import com.shinwootns.swt.property.SystemProperties;
 
 
 @Controller
 public class ServiceController {
 	
+	@Autowired
+	private SystemProperties properties;
+	
 	@PostConstruct
 	public void startService() {
-		System.out.println("ServiceController - startService()... Start");
+		
+		System.out.println(String.format("ServiceController - startService(%s)... Start", properties.getName()));
+		
+		System.out.println(String.format("ServiceController - startService(%s)... End", properties.getName()));
 	}
 	
 	@PreDestroy
@@ -20,4 +28,4 @@ public class ServiceController {
 		System.out.println("ServiceController - stopService()... end");
 	}
 }
-*/
+
