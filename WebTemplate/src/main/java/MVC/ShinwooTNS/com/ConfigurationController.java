71f1c5cb7ada5a71261761c8706b7d738dac0728
jwl920919 <code_king@naver.com>
2016-07-04 17:01:58 +0900

    Commit

---
+++ WebTemplate/src/main/java/MVC/ShinwooTNS/com/ConfigurationController.java
@@ -0,0 +1,42 @@
+package MVC.ShinwooTNS.com;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.google.gson.Gson;
+
+import Common.DTO.AjaxResult;
+
+@Controller
+@RequestMapping(value = "/configuration/")
+public class ConfigurationController {
+	private final static java.text.SimpleDateFormat SIMPLE_DATE_FORMAT = new java.text.SimpleDateFormat(
+			"yyyy-MM-dd HH:mm:ss");
+	private final static String parentPath = "/configuration/";
+	private static final Logger logger = LoggerFactory.getLogger(ConfigurationController.class);
+	private Gson gson = new Gson();
+	private AjaxResult result = new AjaxResult();
+
+	@RequestMapping(value = "systemUserInfo", method = RequestMethod.GET)
+	public String Main(Locale locale, Model model, HttpServletRequest request, HttpServletResponse response) {
+		logger.info("systemUserInfo : " + request.getLocalAddr());
+		System.out.println("systemUserInfo Controller");
+		// Session에 로그인 정보가 있는지 체크
+		HttpSession session = request.getSession(true);
+		System.out.println(session.getAttribute("login_chk"));
+		if (session.getAttribute("login_chk") == null)
+			return "redirect:login";
+
+		return parentPath + "systemUserInfo";
+	}
+}
