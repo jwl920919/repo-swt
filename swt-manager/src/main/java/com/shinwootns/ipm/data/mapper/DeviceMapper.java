61d9e89c419c7541b94e3dfb78069355892cd526
songagi <songagi@gmail.com>
2016-07-06 13:16:13 +0900

    Modify back-end

---
+++ swt-manager/src/main/java/com/shinwootns/ipm/data/mapper/DeviceMapper.java
@@ -0,0 +1,34 @@
+package com.shinwootns.ipm.data.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.shinwootns.ipm.data.entity.DeviceInfo;
+
+@Mapper
+public interface DeviceMapper {
+
+	@Select(""
+		+ "SELECT dev.* FROM device_info dev, SITE_INFO site"
+		+ "WHERE dev.site_id = site.site_id"
+		+ "AND dev.device_type = #{device_type}"
+	)
+	List<DeviceInfo> selectDevice();
+	
+	
+	@Update(""
+		+ " UPDATE device_info"
+		+ " SET	device_name=#{device_name}"
+		+ " , vendor=#{vendor}"
+		+ " , model=#{model}"
+		+ " , service_type=#{service_type}"
+		+ " , sys_oid=#{sys_oid}"
+		+ " , sys_location=#{sys_location}"
+		+ " , update_time = now()"
+		+ " WHERE device_id = #{device_id}"
+    )
+	void updateDevice(DeviceInfo deviceInfo);
+}
