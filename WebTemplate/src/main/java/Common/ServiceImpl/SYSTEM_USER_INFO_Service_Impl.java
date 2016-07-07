64b697e8172989837449d87ac9c5e53987c0bcff
cmkim <cmkim@Shinwoo_cmKim>
2016-07-07 17:02:15 +0900

    commit

--- WebTemplate/src/main/java/Common/ServiceImpl/SYSTEM_USER_INFO_Service_Impl.java
+++ WebTemplate/src/main/java/Common/ServiceImpl/SYSTEM_USER_INFO_Service_Impl.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 
 import Common.DAOInterface.SYSTEM_USER_INFO_Interface;
+import Common.DTO.SYSTEM_USER_INFO_DTO;
 import Common.ServiceInterface.SYSTEM_USER_INFO_Service_Interface;
 
 public class SYSTEM_USER_INFO_Service_Impl implements SYSTEM_USER_INFO_Service_Interface {
@@ -16,5 +17,16 @@ public class SYSTEM_USER_INFO_Service_Impl implements SYSTEM_USER_INFO_Service_I
 	public List<Common.DTO.SYSTEM_USER_INFO_DTO> select_SYSTEM_USER_INFO() {
 		// TODO Auto-generated method stub
 		return userInfoDao.select_SYSTEM_USER_INFO();
+	}
+
+	@Override
+	public List<SYSTEM_USER_INFO_DTO> select_SYSTEM_USER_INFO_CONDITIONAL_SEARCH(HashMap<String, Object> parameters) {
+		// TODO Auto-generated method stub
+		return userInfoDao.select_SYSTEM_USER_INFO_CONDITIONAL_SEARCH(parameters);
+	}
+
+	@Override
+	public int select_SYSTEM_USER_INFO_CONDITIONAL_SEARCH_TOTAL_COUNT(HashMap<String, Object> parameters) {
+		return userInfoDao.select_SYSTEM_USER_INFO_CONDITIONAL_SEARCH_TOTAL_COUNT(parameters);
 	}
 }
--- WebTemplate/src/main/webapp/resources/js/common/Common.js
+++ WebTemplate/src/main/webapp/resources/js/common/Common.js
@@ -56,3 +56,43 @@ String.format = function() {
 	}
 	return s;
 }
+
+// DateTime format function
+/**
+*-2011년 09월 11일 오후 03시 45분 42초
+*console.log(new Date().format("yyyy년 MM월 dd일 a/p hh시 mm분 ss초")); 
+*-2011-09-11
+*console.log(new Date().format("yyyy-MM-dd")); 
+*-'11 09.11
+*console.log(new Date().format("'yy MM.dd")); 
+*-2011-09-11 일요일
+*console.log(new Date().format("yyyy-MM-dd E")); 
+*-현재년도 : 2011
+*console.log("현재년도 : " + new Date().format("yyyy"));
+**/
+Date.prototype.format = function(f) {
+    if (!this.valueOf()) return " ";
+    
+    var weekName = [getLanguage("sunday"), getLanguage("monday"), getLanguage("tuesday"), getLanguage("wednesday"),
+                    getLanguage("thursday"), getLanguage("friday"), getLanguage("saturday")];
+    var d = this;
+     
+    return f.replace(/(yyyy|yy|MM|dd|E|hh|mm|ss|a\/p)/gi, function($1) {
+        switch ($1) {
+            case "yyyy": return d.getFullYear();
+            case "yy": return (d.getFullYear() % 1000).zf(2);
+            case "MM": return (d.getMonth() + 1).zf(2);
+            case "dd": return d.getDate().zf(2);
+            case "E": return weekName[d.getDay()];
+            case "HH": return d.getHours().zf(2);
+            case "hh": return ((h = d.getHours() % 12) ? h : 12).zf(2);
+            case "mm": return d.getMinutes().zf(2);
+            case "ss": return d.getSeconds().zf(2);
+            case "a/p": return d.getHours() < 12 ? getLanguage("am") : getLanguage("pm");
+            default: return $1;
+        }
+    });
+}; 
+String.prototype.string = function(len){var s = '', i = 0; while (i++ < len) { s += this; } return s;};
+String.prototype.zf = function(len){return "0".string(len - this.length) + this;};
+Number.prototype.zf = function(len){return this.toString().zf(len);};
