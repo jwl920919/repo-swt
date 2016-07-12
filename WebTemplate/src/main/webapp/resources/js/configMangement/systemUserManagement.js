d694a959eb5d67304e31606e414bdff4d5e9a018
jwl920919 <code_king@naver.com>
2016-07-12 20:24:24 +0900

    commit

--- WebTemplate/src/main/webapp/resources/js/configMangement/systemUserManagement.js
+++ WebTemplate/src/main/webapp/resources/js/configMangement/systemUserManagement.js
@@ -2,6 +2,8 @@ var table;
 $(document)
         .ready(
                 function() {
+                    // alert screen 감추기
+                    $("#layDiv").css("visibility", "hidden");
                     table = $('#datatable')
                             .DataTable(
                                     {
@@ -68,33 +70,33 @@ $('#checkbox_controller').click(function() {
 });
 
 $('#delete-button').click(function() {
-    systemAlert("divAlert");
-//    var rows = $("input[name=checkbox-active]:checkbox:checked");
-//    var jsonArray = new Array();
-//    for (var i = 0; i < rows.length; i++) {
-//        var tr = $(rows[i]).parent().parent();
-//        var td = tr.children().next(); // time
-//        var jObj = Object();
-//        jObj.user_id = td.html();
-//        jsonArray.push(jObj);
-//    }
-//    var jsonInfo = JSON.stringify(jsonArray);
-//    $.ajax({
-//        url : "configManagement/deleteUsers",
-//        type : "POST",
-//        data : jsonInfo,
-//        dataType : "text",
-//        success : function(data) {
-//            var jsonObj = eval("(" + data + ')');
-//            if (jsonObj.result == true) {
-//                table.ajax.reload();
-//                console.log('삭제 성공');
-//            } else {
-//                console.log('삭제 실패');
-//            }
-//        }
-//    });
-    
+    systemAlert("divAlertArea", "alert-danger", "삭제", "정말 유저 정보를 삭제하시겠습니까?");
+    // var rows = $("input[name=checkbox-active]:checkbox:checked");
+    // var jsonArray = new Array();
+    // for (var i = 0; i < rows.length; i++) {
+    // var tr = $(rows[i]).parent().parent();
+    // var td = tr.children().next(); // time
+    // var jObj = Object();
+    // jObj.user_id = td.html();
+    // jsonArray.push(jObj);
+    // }
+    // var jsonInfo = JSON.stringify(jsonArray);
+    // $.ajax({
+    // url : "configManagement/deleteUsers",
+    // type : "POST",
+    // data : jsonInfo,
+    // dataType : "text",
+    // success : function(data) {
+    // var jsonObj = eval("(" + data + ')');
+    // if (jsonObj.result == true) {
+    // table.ajax.reload();
+    // console.log('삭제 성공');
+    // } else {
+    // console.log('삭제 실패');
+    // }
+    // }
+    // });
+
 });
 
 // switching [ add(1), modify(2) ]
