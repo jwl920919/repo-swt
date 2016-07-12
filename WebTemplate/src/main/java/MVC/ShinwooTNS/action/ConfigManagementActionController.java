129ab905a97ab7f2e9fdd6fb4f0cdefae9c2cdaf
jwl920919 <code_king@naver.com>
2016-07-12 16:51:52 +0900

    commit

--- WebTemplate/src/main/java/MVC/ShinwooTNS/action/ConfigManagementActionController.java
+++ WebTemplate/src/main/java/MVC/ShinwooTNS/action/ConfigManagementActionController.java
@@ -123,6 +123,7 @@ public class ConfigManagementActionController {
 			}.getType());
 			int group_id = Integer.parseInt(map.get("group_id").toString()),
 					site_id = Integer.parseInt(map.get("site_id").toString());
+			String time_zone = map.get("time_zone").toString();
 			map.remove("group_id");
 			map.remove("site_id");
 			map.put("group_id", group_id);
