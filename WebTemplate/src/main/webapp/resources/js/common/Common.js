441fc24b1d6ab612a77cb04c4e9789eb6541a3d7
jwl920919 <code_king@naver.com>
2016-07-22 14:33:05 +0900

    commit

--- WebTemplate/src/main/webapp/resources/js/common/Common.js
+++ WebTemplate/src/main/webapp/resources/js/common/Common.js
@@ -17,14 +17,14 @@ function systemAlert(divElement, type, title, message) {
     // .alert-info
     // .alert-success
     var tag = "";
-    tag += "<div class='alert " + type
+    tag += "<div id='custom-alert' class='alert " + type
             + " alert-dismissible fade in' role='alert'>";
     tag += "     <button onclick='fnAlertClose(\"layDiv\")' type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>";
     tag += "     <h4><i class='icon fa fa-warning'></i><label>" + title
             + "</label></h4>";
     tag += "     <label >" + message + "</label> ";
     tag += "</div> ";
-
+    $("#" + divElement).html('');
     $("#" + divElement).append(tag);
 
 }
