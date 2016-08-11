d47c90ca41f3d21d8979ee622331e14375ea58e7
cmkim <cmkim@Shinwoo_cmKim>
2016-08-11 15:45:55 +0900

    1

--- WebTemplate/src/main/java/MVC/ShinwooTNS/com/PageController.java
+++ WebTemplate/src/main/java/MVC/ShinwooTNS/com/PageController.java
@@ -5,52 +5,42 @@ import java.io.PrintWriter;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.xml.crypto.Data;
 
-import org.json.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.log4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.google.gson.Gson;
 
-import Common.Controller.SessionLocaleController;
 import Common.DTO.AjaxResult;
 import Common.Helper.CommonHelper;
+import Common.Helper.ErrorLoggingHelper;
 import Common.Helper.LanguageHelper;
 import Common.ServiceInterface.UI_MENU_MASTER_Service_Interface;
-import net.sf.json.JSON;
-import scala.annotation.meta.setter;
 
 @Controller
 public class PageController {
 	private final static java.text.SimpleDateFormat SIMPLE_DATE_FORMAT = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-	private static final Logger logger = LoggerFactory.getLogger(PageController.class);
+	private static final Logger logger = Logger.getLogger(PageController.class);
 	private Gson gson = new Gson();		
 	private AjaxResult result = new AjaxResult();
 	
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String Initialize(Locale locale, Model model, HttpServletRequest request, HttpServletResponse response,
-			HttpSession session) throws IOException {
-		logger.info("Initialize : " + request.getLocalAddr());
+	public String Initialize(Locale locale, Model model, HttpServletRequest request, HttpServletResponse response, HttpSession session) throws IOException {
+		//logger.info("Initialize : " + request.getLocalAddr());
 		System.out.println("Initialize Controller");
 
 		// 암복호화 테스트
@@ -61,19 +51,23 @@ public class PageController {
 
 		// region 다국어 Class Instance
 		// 각자 다국어 설정을 체크 필요
-		String strFileName = (new CommonHelper()).getCookies(request, "Language");
-		if (strFileName != null) {
-			LanguageHelper.getInstance(strFileName);
-		} else {
-			String sLanguageValue = LanguageHelper.getInstance(locale).getFileName();
-			Cookie cookie = new Cookie("Language", sLanguageValue);
-			cookie.setMaxAge(365 * 24 * 60 * 60); // 1년
-			response.addCookie(cookie);
+		try {
+			String strFileName = (new CommonHelper()).getCookies(request, "Language");
+			if (strFileName != null) {
+				LanguageHelper.getInstance(strFileName);
+			} else {
+				String sLanguageValue = LanguageHelper.getInstance(locale).getFileName();
+				Cookie cookie = new Cookie("Language", sLanguageValue);
+				cookie.setMaxAge(365 * 24 * 60 * 60); // 1년
+				response.addCookie(cookie);
+			}
+		} catch (Exception e) {
+			ErrorLoggingHelper.log(logger, "Initialize Controller", e);
 		}
 		// endregion 다국어 Class Instance
 
 		// region Login Session check
-		System.out.println(MessageFormat.format("Login Check : {0}", session.getAttribute("login_chk")));
+		//System.out.println(MessageFormat.format("Login Check : {0}", session.getAttribute("login_chk")));
 
 		if (session.getAttribute("login_chk") != null)
 			return "redirect:/main";
@@ -87,7 +81,7 @@ public class PageController {
 
 	@RequestMapping(value = "/main", method = RequestMethod.GET)
 	public String Main(Locale locale, Model model, HttpServletRequest request, HttpServletResponse response) {
-		logger.info("Main : " + request.getLocalAddr());
+		//logger.info("Main : " + request.getLocalAddr());
 		System.out.println("Main Controller");
 		logger.info("Join User IP : " + request.getRemoteAddr());
 		System.out.println("");
@@ -106,78 +100,82 @@ public class PageController {
 		// listMenu = myappService.selectTestTable();
 
 		// region 메뉴 데이터 생성
-		StringBuilder sb = new StringBuilder();
-		String sMasterCD = null;
-		String sSubCD = null;
-		String sMasterNameKey = null;
-		String ssubNameKey = null;
-		for (int i = 0; i < listMenu.size(); i++) {
-			System.out.println(listMenu.get(i));
-			// System.out.println(listMenu.get(i).get("sSubDiscription"));
-
-			if (sMasterCD == null) {
-				sMasterCD = listMenu.get(i).get("master_cd").toString();
-				sMasterNameKey = listMenu.get(i).get("master_namekey").toString();
-				ssubNameKey = listMenu.get(i).get("subname_key").toString();
-				sb.append("<li class='active treeview' id='menu_" + sMasterCD +"'>");
-//				sb.append("	<a href='" + listMenu.get(i).get("sMasterURL").toString() + "'>");
-				sb.append("	<a onclick='changeframe(&quot;" + listMenu.get(i).get("master_url").toString() + "&quot;, &quot;" + sMasterCD + "&quot;, &quot;" + sMasterNameKey + "&quot;, &quot;" + ssubNameKey + "&quot;);' style='cursor:pointer' >");
-				sb.append("		<i class='fa " + listMenu.get(i).get("master_icon").toString() + "'></i>");
-				sb.append("		<span>" + LanguageHelper.GetLanguage(listMenu.get(i).get("master_namekey").toString())
-						+ "</span>");
-				if (sMasterCD.equals("M01")) {
-					sb.append("			</a></li>");
+		try {			
+			StringBuilder sb = new StringBuilder();
+			String sMasterCD = null;
+			String sSubCD = null;
+			String sMasterNameKey = null;
+			String ssubNameKey = null;
+			for (int i = 0; i < listMenu.size(); i++) {
+				System.out.println(listMenu.get(i));
+				// System.out.println(listMenu.get(i).get("sSubDiscription"));
+	
+				if (sMasterCD == null) {
+					sMasterCD = listMenu.get(i).get("master_cd").toString();
+					sMasterNameKey = listMenu.get(i).get("master_namekey").toString();
+					ssubNameKey = listMenu.get(i).get("subname_key").toString();
+					sb.append("<li class='active treeview' id='menu_" + sMasterCD +"'>");
+	//				sb.append("	<a href='" + listMenu.get(i).get("sMasterURL").toString() + "'>");
+					sb.append("	<a onclick='changeframe(&quot;" + listMenu.get(i).get("master_url").toString() + "&quot;, &quot;" + sMasterCD + "&quot;, &quot;" + sMasterNameKey + "&quot;, &quot;" + ssubNameKey + "&quot;);' style='cursor:pointer' >");
+					sb.append("		<i class='fa " + listMenu.get(i).get("master_icon").toString() + "'></i>");
+					sb.append("		<span>" + LanguageHelper.GetLanguage(listMenu.get(i).get("master_namekey").toString())
+							+ "</span>");
+					if (sMasterCD.equals("M01")) {
+						sb.append("			</a></li>");
+					} else {
+						sSubCD = listMenu.get(i).get("sub_cd").toString();
+						sb.append("		<i class='fa fa-angle-left pull-right'></i></a>");
+						sb.append("	<ul class='treeview-menu' name='ulMenu'>");
+						sb.append("		<li class='active' id='menu_" + sSubCD +"'>");
+	//					sb.append("			<a href='" + listMenu.get(i).get("sSubUrl").toString() + "'>");
+						sb.append("			<a onclick='changeframe(&quot;" + listMenu.get(i).get("sub_Url").toString() + "&quot;, &quot;" + sSubCD + "&quot;, &quot;" + sMasterNameKey + "&quot;, &quot;" + ssubNameKey + "&quot;);' style='cursor:pointer' >");
+						sb.append("				<i class='fa fa-circle-o'></i>");
+						sb.append(LanguageHelper.GetLanguage(listMenu.get(i).get("subname_key").toString()));
+						sb.append("			</a></li>");
+					}
+				} else if (sMasterCD.equals(listMenu.get(i).get("master_cd").toString())) {
+					// 같은 Master 메뉴 처리
+					if (!sMasterCD.equals("M01")) {
+						sSubCD = listMenu.get(i).get("sub_cd").toString();
+						ssubNameKey = listMenu.get(i).get("subname_key").toString();
+						sb.append("		<li id='menu_" + sSubCD +"'>");
+	//					sb.append("			<a href='" + listMenu.get(i).get("sSubUrl").toString() + "'>");
+						sb.append("			<a onclick='changeframe(&quot;" + listMenu.get(i).get("sub_Url").toString() + "&quot;, &quot;" + sSubCD + "&quot;, &quot;" + sMasterNameKey + "&quot;, &quot;" + ssubNameKey + "&quot;);' style='cursor:pointer' >");
+						sb.append("				<i class='fa fa-circle-o'></i>");
+						sb.append(LanguageHelper.GetLanguage(listMenu.get(i).get("subname_key").toString()));
+						sb.append("			</a></li>");
+					}
 				} else {
+					// 다른 Master 메뉴 처리
+					sMasterCD = listMenu.get(i).get("master_cd").toString();
 					sSubCD = listMenu.get(i).get("sub_cd").toString();
+					sMasterNameKey = listMenu.get(i).get("master_namekey").toString();
+					ssubNameKey = listMenu.get(i).get("subname_key").toString();
+					sb.append("	</ul>");
+					sb.append("</li>");
+					sb.append("<li class='treeview' id='menu_" + sMasterCD +"'>");
+	//				sb.append("	<a href='" + listMenu.get(i).get("sMasterURL").toString() + "'>");
+					sb.append("	<a style='cursor:pointer' >");
+					sb.append("		<i class='fa " + listMenu.get(i).get("master_icon").toString() + "'></i>");
+					sb.append("		<span>" + LanguageHelper.GetLanguage(listMenu.get(i).get("master_namekey").toString())
+							+ "</span>");
 					sb.append("		<i class='fa fa-angle-left pull-right'></i></a>");
 					sb.append("	<ul class='treeview-menu' name='ulMenu'>");
 					sb.append("		<li class='active' id='menu_" + sSubCD +"'>");
-//					sb.append("			<a href='" + listMenu.get(i).get("sSubUrl").toString() + "'>");
+	//				sb.append("			<a href='" + listMenu.get(i).get("sSubUrl").toString() + "'>");
 					sb.append("			<a onclick='changeframe(&quot;" + listMenu.get(i).get("sub_Url").toString() + "&quot;, &quot;" + sSubCD + "&quot;, &quot;" + sMasterNameKey + "&quot;, &quot;" + ssubNameKey + "&quot;);' style='cursor:pointer' >");
 					sb.append("				<i class='fa fa-circle-o'></i>");
 					sb.append(LanguageHelper.GetLanguage(listMenu.get(i).get("subname_key").toString()));
 					sb.append("			</a></li>");
 				}
-			} else if (sMasterCD.equals(listMenu.get(i).get("master_cd").toString())) {
-				// 같은 Master 메뉴 처리
-				if (!sMasterCD.equals("M01")) {
-					sSubCD = listMenu.get(i).get("sub_cd").toString();
-					ssubNameKey = listMenu.get(i).get("subname_key").toString();
-					sb.append("		<li id='menu_" + sSubCD +"'>");
-//					sb.append("			<a href='" + listMenu.get(i).get("sSubUrl").toString() + "'>");
-					sb.append("			<a onclick='changeframe(&quot;" + listMenu.get(i).get("sub_Url").toString() + "&quot;, &quot;" + sSubCD + "&quot;, &quot;" + sMasterNameKey + "&quot;, &quot;" + ssubNameKey + "&quot;);' style='cursor:pointer' >");
-					sb.append("				<i class='fa fa-circle-o'></i>");
-					sb.append(LanguageHelper.GetLanguage(listMenu.get(i).get("subname_key").toString()));
-					sb.append("			</a></li>");
-				}
-			} else {
-				// 다른 Master 메뉴 처리
-				sMasterCD = listMenu.get(i).get("master_cd").toString();
-				sSubCD = listMenu.get(i).get("sub_cd").toString();
-				sMasterNameKey = listMenu.get(i).get("master_namekey").toString();
-				ssubNameKey = listMenu.get(i).get("subname_key").toString();
-				sb.append("	</ul>");
-				sb.append("</li>");
-				sb.append("<li class='treeview' id='menu_" + sMasterCD +"'>");
-//				sb.append("	<a href='" + listMenu.get(i).get("sMasterURL").toString() + "'>");
-				sb.append("	<a style='cursor:pointer' >");
-				sb.append("		<i class='fa " + listMenu.get(i).get("master_icon").toString() + "'></i>");
-				sb.append("		<span>" + LanguageHelper.GetLanguage(listMenu.get(i).get("master_namekey").toString())
-						+ "</span>");
-				sb.append("		<i class='fa fa-angle-left pull-right'></i></a>");
-				sb.append("	<ul class='treeview-menu' name='ulMenu'>");
-				sb.append("		<li class='active' id='menu_" + sSubCD +"'>");
-//				sb.append("			<a href='" + listMenu.get(i).get("sSubUrl").toString() + "'>");
-				sb.append("			<a onclick='changeframe(&quot;" + listMenu.get(i).get("sub_Url").toString() + "&quot;, &quot;" + sSubCD + "&quot;, &quot;" + sMasterNameKey + "&quot;, &quot;" + ssubNameKey + "&quot;);' style='cursor:pointer' >");
-				sb.append("				<i class='fa fa-circle-o'></i>");
-				sb.append(LanguageHelper.GetLanguage(listMenu.get(i).get("subname_key").toString()));
-				sb.append("			</a></li>");
 			}
+			sb.append("	</ul>");
+			sb.append("</li>");
+			System.out.println(sb.toString());
+			model.addAttribute("menuHTML", sb.toString());
+		} catch (Exception e) {
+			ErrorLoggingHelper.log(logger, "main Menu Make", e);
 		}
-		sb.append("	</ul>");
-		sb.append("</li>");
-		System.out.println(sb.toString());
-		model.addAttribute("menuHTML", sb.toString());
 		// endregion 메뉴 데이터 생성
 
 		return "/main";
