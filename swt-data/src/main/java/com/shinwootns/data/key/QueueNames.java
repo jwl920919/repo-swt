33892b07afd10191d314c740847888912e5662a8
songagi <songagi@gmail.com>
2016-08-17 19:17:41 +0900

    .

--- swt-data/src/main/java/com/shinwootns/data/key/QueueNames.java
+++ swt-data/src/main/java/com/shinwootns/data/key/QueueNames.java
@@ -3,6 +3,6 @@ package com.shinwootns.data.key;
 public class QueueNames {
 	
 	public final static String EVENT_QUEUE_NAME		= "ipm.event";
-	public final static String DHCP_QUEUE_NAME		= "ipm.dhcp";
+	//public final static String DHCP_QUEUE_NAME		= "ipm.dhcp";
 	
 }
